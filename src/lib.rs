8aaa46a492f2e1463f09c2894e37f01f99a72bc8
Mick van Gelderen <mickvangelderen@gmail.com>
2017-10-17 14:36:43 +0200

    Add cooler numeric types

--- src/lib.rs
+++ src/lib.rs
@@ -1,122 +1,116 @@
 use ::std::marker::PhantomData;
 use ::std::ops;
-use ::std::cmp;
-use ::std::fmt;
-use ::std::convert;
+// use ::std::cmp;
+// use ::std::fmt;
+// use ::std::convert;
 
-pub enum N3 {}
-pub enum N2 {}
-pub enum N1 {}
-pub enum Z0 {}
-pub enum P1 {}
-pub enum P2 {}
-pub enum P3 {}
+struct P<T>(PhantomData<T>);
+struct Z;
+struct N<T>(PhantomData<T>);
 
-pub struct Quantity<V, D1, D2>(V, PhantomData<(D1, D2)>);
+// TODO: Wouldn't this be cleaner?
+// trait TypeAdd<RHS = Self> {
+//     type Output;
+// }
+// impl TypeAdd for Z {
+//     type Output = Z;
+// }
 
-pub type Length<V> = Quantity<V, P1, Z0>;
-
-pub type Time<V> = Quantity<V, Z0, P1>;
-
-pub type Velocity<V> = Quantity<V, P1, N1>;
-
-macro_rules! impl_dim_mul {
-    ($L:ident * $R:ident = $O:ident) => (
-        impl ops::Mul<$R> for $L {
-            type Output = $O;
-            fn mul(self, _: $R) -> Self::Output {
-                unreachable!()
-            }
-        }
-    )
-}
-
-// impl_dim_mul!(N3 * N3 = N6);
-// impl_dim_mul!(N3 * N2 = N5);
-// impl_dim_mul!(N3 * N1 = N4);
-impl_dim_mul!(N3 * Z0 = N3);
-impl_dim_mul!(N3 * P1 = N2);
-impl_dim_mul!(N3 * P2 = N1);
-impl_dim_mul!(N3 * P3 = Z0);
-
-macro_rules! impl_dim_div {
-    ($L:ty, $R:ty, $O:ty) => (
-        impl ops::Div<$R> for $L {
-            type Output = $O;
-            fn div(self, _: $R) -> Self::Output {
-                unreachable!()
-            }
-        }
-    );
-}
-
-impl_dim_div!(N3, N3, Z0);
-impl_dim_div!(N3, N2, N1);
-impl_dim_div!(N3, N1, N2);
-impl_dim_div!(N3, Z0, N3);
-// impl_dim_div!(N3, P1, N4);
-// impl_dim_div!(N3, P2, N5);
-// impl_dim_div!(N3, P3, N6);
-
-impl_dim_div!(N2, N3, P1);
-impl_dim_div!(N2, N2, Z0);
-impl_dim_div!(N2, N1, N1);
-impl_dim_div!(N2, Z0, N2);
-impl_dim_div!(N2, P1, N3);
-// impl_dim_div!(N2, P2, N4);
-// impl_dim_div!(N2, P3, N5);
-
-impl_dim_div!(N1, N3, P2);
-impl_dim_div!(N1, N2, P1);
-impl_dim_div!(N1, N1, Z0);
-impl_dim_div!(N1, Z0, N1);
-impl_dim_div!(N1, P1, N2);
-impl_dim_div!(N1, P2, N3);
-// impl_dim_div!(N1, P3, N4);
-
-impl ops::Div<Z0> for P1 {
-    type Output = N1;
-    fn div(self, _: Z0) -> Self::Output {
-        unreachable!()
+// 0 + 0 (additive identity)
+impl ops::Add for Z {
+    type Output = Z;
+    fn add(self, _: Z) -> Self::Output {
+        unreachable!();
     }
 }
 
-impl ops::Div<P1> for Z0 {
-    type Output = Z0;
-    fn div(self, _: P1) -> Self::Output {
-        unreachable!()
+// v + 0 (additive identity)
+impl<T> ops::Add<Z> for P<T> {
+    type Output = Self;
+    fn add(self, _: Z) -> Self::Output {
+        unreachable!();
     }
 }
 
-impl<V: ops::Div, Q1D1, Q1D2, Q2D1, Q2D2> ops::Div<Quantity<V, Q2D1, Q2D2>> for Quantity<V, Q1D1, Q1D2>
-    where V: ops::Div,
-          Q1D1: ops::Div<Q2D1>,
-          Q1D2: ops::Div<Q2D2>,
-{
-    type Output = Quantity<
-            V::Output,
-            <Q1D1 as ops::Div<Q2D1>>::Output,
-            <Q1D2 as ops::Div<Q2D2>>::Output
-        >;
-
-    fn div(self, other: Quantity<V, Q2D1, Q2D2>) -> Self::Output {
-        (self.0/other.0).into()
+// -v + 0 (additive identity)
+impl<T> ops::Add<Z> for N<T> {
+    type Output = Self;
+    fn add(self, _: Z) -> Self::Output {
+        unreachable!();
     }
 }
 
-impl<V, D1, D2> convert::From<V> for Quantity<V, D1, D2> {
-    fn from(v: V) -> Self {
-        Quantity(v, PhantomData)
+// 0 + v (additive identity)
+impl<T> ops::Add<P<T>> for Z {
+    type Output = P<T>;
+    fn add(self, _: P<T>) -> Self::Output {
+        unreachable!();
     }
 }
+
+// 0 + -1 (additive identity)
+impl<T> ops::Add<N<T>> for Z {
+    type Output = N<T>;
+    fn add(self, _: N<T>) -> Self::Output {
+        unreachable!();
+    }
+}
+
+// v + v (addition)
+impl <TL: ops::Add<TR>, TR> ops::Add<P<TR>> for P<TL> {
+    type Output = P<P<<TL as ops::Add<TR>>::Output>>;
+    fn add(self, _: P<TR>) -> Self::Output {
+        unreachable!();
+    }
+}
+
+// -v + v (addition)
+impl <TL: ops::Add<TR>, TR> ops::Add<P<TR>> for N<TL> {
+    type Output = <TL as ops::Add<TR>>::Output;
+    fn add(self, _: P<TR>) -> Self::Output {
+        unreachable!();
+    }
+}
+
+// v + -v (addition)
+impl <TL: ops::Add<TR>, TR> ops::Add<N<TR>> for P<TL> {
+    type Output = <TL as ops::Add<TR>>::Output;
+    fn add(self, _: N<TR>) -> Self::Output {
+        unreachable!();
+    }
+}
+
+// -v + -v (addition)
+impl <TL: ops::Add<TR>, TR> ops::Add<N<TR>> for N<TL> {
+    type Output = N<N<<TL as ops::Add<TR>>::Output>>;
+    fn add(self, _: N<TR>) -> Self::Output {
+        unreachable!();
+    }
+}
+
+// Types representing the positive numbers 1, 2 and 3.
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    type N1 = N<Z>;
+    type N2 = N<N1>;
+    type N3 = N<N2>;
+
+    type P1 = P<Z>;
+    type P2 = P<P1>;
+    type P3 = P<P2>;
+
     #[test]
     fn it_works() {
-        let m1: Length<_> = 10f32.into();
-        let m2 = Length::from(40f32);
+        // Run time addition. (panics)
+        let _x: P3 = P1 { 0: PhantomData } + P2 { 0: PhantomData };
+
+        // Compile time addition is the type of _y.
+        let _y: <P1 as ops::Add<P2>>::Output = _x;
+
+        // Hahahah this works!!!
+        let _x: Z = N1 { 0: PhantomData } + P1 { 0: PhantomData };
     }
 }
